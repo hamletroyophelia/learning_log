ad7ca566f871bbaf269ec35e6c0bac42116686b5
Hamletroy <hamletroy1@outlook.com>
2023-07-20 21:25:55 +0800

    数据库配置

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'll_env_db',
         'USER': 'admin',
-        'PASSWORD': 'ly112358',
+        'PASSWORD': 'sIi5oxgu7hCMs8ozNdLp1xDpPzkLFfW1',
     }
 }
 
@@ -134,3 +134,4 @@ LOGIN_URL = 'users:login'
 import django_heroku
 django_heroku.settings(locals())
 """
+#render设置
